6ea1b9b51add5a3a090e459a6a42e67ca2241867
KieuTrang0210 <103953173+KieuTrang0210@users.noreply.github.com>
2023-06-20 19:10:37 +0700

    thiet ke trang chu + xu ly chuc nang dang xuat

---
+++ app/src/main/java/com/example/appbanhangonline/activities/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.example.appbanhangonline.activities;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;;
+
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appbanhangonline.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+
+    //Đăng xuất
+    public void logoutClicked(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Bạn có muốn đăng xuất không?")
+                .setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
--- app/src/main/java/com/example/appbanhangonline/activities/MainActivity.java
+++ app/src/main/java/com/example/appbanhangonline/activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appbanhangonline.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.appbanhangonline.R;
 
@@ -12,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        trang login, đang ví dụ minh hoạ để làm logout
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
