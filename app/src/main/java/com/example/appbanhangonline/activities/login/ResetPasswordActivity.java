6d30d9539b1d72d26b3d1bee6c419619f348d7c5
Thuy Than <2051063434@e.tlu.edu.vn>
2023-06-25 01:43:51 +0700

    update layout

---
+++ app/src/main/java/com/example/appbanhangonline/activities/login/ResetPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.example.appbanhangonline.activities.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.appbanhangonline.R;
+
+public class ResetPasswordActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
