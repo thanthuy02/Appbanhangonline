97319be9a4403c3067062f97cf99ad11833b0c1c
Thuy Than <2051063434@e.tlu.edu.vn>
2023-06-25 20:20:19 +0700

    create product

--- app/src/main/java/com/example/appbanhangonline/adapters/ProductAdapter.java
+++ app/src/main/java/com/example/appbanhangonline/adapters/ProductAdapter.java
@@ -1,6 +1,9 @@
 package com.example.appbanhangonline.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,49 +19,43 @@ import com.example.appbanhangonline.activities.admin.ProductActivity;
 import com.example.appbanhangonline.dbhandler.ProductHandler;
 import com.example.appbanhangonline.models.Product;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class ProductAdapter extends ArrayAdapter<Product> {
 
-    private Context context;
     private ArrayList<Product> products;
-
-    private ProductHandler productHandler;
-
-    @Override
-    public int getCount() {
-        return products.size();
-    }
-
-    public ProductAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
-    }
 
     public ProductAdapter(ProductActivity context, int resource, ArrayList<Product> products) {
         super(context, resource, products);
     }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         if (convertView == null) {
-            LayoutInflater inflater = LayoutInflater.from(context);
+            LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.listview_product_item, parent, false);
         }
 
-        ImageView productImage = convertView.findViewById(R.id.imgProduct);
-        TextView productName = convertView.findViewById(R.id.productName);
-        TextView productCategory = convertView.findViewById(R.id.productCategory);
-        TextView productQuantity = convertView.findViewById(R.id.productQuantity);
-        TextView productPrice = convertView.findViewById(R.id.productPrice);
+        Product product = getItem(position);
+        if (product != null) {
+            ImageView productImage = convertView.findViewById(R.id.imgProduct);
+            TextView productName = convertView.findViewById(R.id.productName);
+            TextView productCategory = convertView.findViewById(R.id.productCategory);
+            TextView productQuantity = convertView.findViewById(R.id.productQuantity);
+            TextView productPrice = convertView.findViewById(R.id.productPrice);
 
-        Product product = products.get(position);
-
-        // Hiển thị thông tin sản phẩm trong các thành phần giao diện
-        Glide.with(context).load(product.getImage()).into(productImage);
-        productName.setText(product.getProductName());
-        productCategory.setText(product.getCategoryName());
-        productQuantity.setText(String.valueOf(product.getQuantity()));
-        productPrice.setText(String.valueOf(product.getPrice()));
+            // Hiển thị thông tin sản phẩm trong các thành phần giao diện
+//            productImage.setImageURI(Uri.parse(product.getImage()));
+            Glide.with(getContext())
+                    .load(Uri.parse(product.getImage()))
+                    .into(productImage);
+            productName.setText(product.getProductName());
+            productCategory.setText(product.getCategoryName());
+            productQuantity.setText(String.valueOf(product.getQuantity()));
+            productPrice.setText(String.valueOf(product.getPrice()));
+        }
 
         return convertView;
     }
