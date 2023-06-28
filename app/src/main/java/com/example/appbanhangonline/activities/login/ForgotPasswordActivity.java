7a2e1c9a4a8b091ffbf5fae1ad14d3ee55c316f9
Thuy Than <2051063434@e.tlu.edu.vn>
2023-06-28 15:08:29 +0700

    send email

--- app/src/main/java/com/example/appbanhangonline/activities/login/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/appbanhangonline/activities/login/ForgotPasswordActivity.java
@@ -6,24 +6,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.appbanhangonline.R;
+import com.example.appbanhangonline.dbhandler.LoginHandler;
+
+import java.util.Random;
 
 public class ForgotPasswordActivity extends Activity {
-    Button btnConfirm;
+    Button btnSendCode;
+    EditText etCustomerEmail;
+    LoginHandler loginHandler;
 
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_verify_code);
-        btnConfirm = (Button) findViewById(R.id.btnConfirm);
+        setContentView(R.layout.activity_forgot_password);
+        loginHandler = new LoginHandler(this);
+        btnSendCode = (Button) findViewById(R.id.btnSendCode);
+        etCustomerEmail = (EditText) findViewById(R.id.etCustomerEmail);
+        Intent intent = getIntent();
+        String userEmail = intent.getStringExtra("userEmail");
+        etCustomerEmail.setText(userEmail);
 
-        btnConfirm.setOnClickListener(new View.OnClickListener() {
+        btnSendCode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                startActivity(new Intent(getApplicationContext(),
-                        ResetPasswordActivity.class));
+            public void onClick(View arg0) {
+                String email = etCustomerEmail.getText().toString().trim();
+                if (!loginHandler.checkEmail(email)) {
+                    Toast.makeText(getApplicationContext(),
+                            "Tài khoản không tồn tại.",
+                            Toast.LENGTH_LONG).show();
+                    etCustomerEmail.setText("");
+                } else {
+                    Random random = new Random();
+                    String code = String.valueOf(random.nextInt(900000) + 100000);
+
+                    loginHandler.sendEmail(email, code);
+                    Intent i = new Intent(getApplicationContext(), VerifyCodeActivity.class);
+                    i.putExtra("code", code);
+                    i.putExtra("email", email);
+                    startActivity(i);
+                    Toast.makeText(getApplicationContext(),
+                            "Đã gửi mã xác nhận",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/appbanhangonline/utils/EmailHelper.java
+++
@@ -1,4 +0,0 @@
-package com.example.appbanhangonline.utils;
-
-public class EmailHelper {
-}
